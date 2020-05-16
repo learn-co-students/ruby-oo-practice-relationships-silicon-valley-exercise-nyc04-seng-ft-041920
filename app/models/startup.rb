146cc5f0d87dec6101cb812493b40dd296c7928f
bashir2017 <bashiralhanshali@yahoo.com>
2020-05-15 22:17:21 -0400

    Done.

--- app/models/startup.rb
+++ app/models/startup.rb
@@ -1,3 +1,67 @@
 class Startup
 
+    @@all = []
+
+    attr_reader :name, :founder, :domain 
+
+    def initialize(name, founder, domain)
+        @name = name 
+        @founder = founder 
+        @domain = domain 
+        save 
+    end 
+
+    def save 
+        @@all << self 
+    end 
+
+    def pivot(domain, name)
+        @domain = domain 
+        @name = name 
+    end 
+
+    def self.all 
+        @@all 
+    end 
+
+    def self.find_by_founder(founder_name)
+        self.all.select{|company| company.founder == founder_name}
+    end 
+
+    def self.domains 
+        self.all.map{|company| company.domain}.uniq 
+    end 
+    
+    def sign_contract(venture_capitalist, type, investment)
+        FundingRound.new(self, venture_capitalist, type, investment)
+    end 
+
+    #returns all funding rounds associated with this startup
+    def funding_rounds 
+        FundingRound.all.select{|f_round| f_round.startup == self}
+    end 
+
+    def num_funding_rounds
+        funding_rounds.count 
+    end 
+
+    def total_funds 
+        funding_rounds.reduce(0){|sum, f_round| sum += f_round.investment}
+    end 
+
+    def investors 
+        funding_rounds.map{|f_round| f_round.venture_capitalist}.uniq 
+    end 
+
+    #refactored 
+    def big_investors 
+        funding_rounds.map{ |f_round| 
+        
+            if f_round.venture_capitalist.total_worth > 1000000000
+                f_round.venture_capitalist
+            end 
+        }.compact 
+    end 
+
+
 end
--- tools/console.rb
+++ tools/console.rb
@@ -3,6 +3,24 @@ require_relative '../config/environment.rb'
 def reload
   load 'config/environment.rb'
 end
+
+
+facebook = Startup.new("Facebook", "Zuck", "social")
+medLee = Startup.new("MedLee", "Joe", "health")
+apple = Startup.new("Apple", "Steve", "Tech")
+uHealth = Startup.new("Nike", "Steve", "health")
+
+
+vc1 = VentureCapitalist.new("Bashir", 1000000000)
+vc2 = VentureCapitalist.new("Mousa", 2000000000)
+
+#startup, venture_capitalist, type, investment
+fr1 = FundingRound.new(facebook, vc1, "Angel", 500.00)
+fr2 = FundingRound.new(apple, vc1, "Angel", 800.00)
+fr3 = FundingRound.new(uHealth, vc2, "Angel", 100425000.00)
+fr1 = FundingRound.new(facebook, vc2, "Angel", 1000.00)
+fr4 = FundingRound.new(medLee, vc2, "Angel", 23452345.00)
+
 # Insert code here to run before hitting the binding.pry
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
