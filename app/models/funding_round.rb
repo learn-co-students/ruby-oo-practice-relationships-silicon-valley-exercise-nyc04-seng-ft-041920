32119d0b0bf9134ddbeb05d17ddc2de9b9c2410b
frod25 <frod25>
2020-05-18 15:20:24 -0400

    Done.

--- app/models/funding_round.rb
+++ app/models/funding_round.rb
@@ -1,3 +1,33 @@
 class FundingRound
+    attr_reader :startup, :venture_capitalist, :investment
+    attr_accessor :type
+
+    @@all = []
+
+    def initialize(startup, venture_capitalist, type, investment=0)
+        @startup = startup
+        @venture_capitalist = venture_capitalist
+        @type = type
+        if investment > 0
+            @investment = investment.to_f
+        else
+            @investment = 0
+        end
+        save
+    end
+
+    def save
+        @@all << self
+    end
+
+    def investment=(amount)
+        if amount > 0
+            @investment = amount.to_f
+        end
+    end
+
+    def self.all 
+        @@all
+    end
 
 end
--- app/models/startup.rb
+++ app/models/startup.rb
@@ -1,3 +1,54 @@
 class Startup
+    attr_reader :name, :founder, :domain
+    attr_accessor 
 
+    @@all = []
+
+    def initialize(name, founder, domain)
+        @name = name
+        @domain = domain
+        @founder = founder
+        @@all << self
+    end
+
+    def pivot(domain, name)
+        @domain = domain
+        @name = name
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.find_by_founder(name)
+        self.all.find {|startup| startup.founder == name}
+    end
+
+    def self.domains
+        self.all.collect {|startup| startup.domain}.uniq
+    end  
+
+    def sign_contract(vc, type, amount)
+        FundingRound.new(self, vc, type, amount.to_f)
+    end
+
+    def get_funding_rounds
+        FundingRound.all.select {|round| round.startup == self}
+    end
+
+    def num_funding_rounds
+        self.get_funding_rounds.count
+    end
+
+    def total_funds
+        self.get_funding_rounds.reduce(0) {|total, round| total + round.investment}
+    end
+
+    def investors
+        self.get_funding_rounds.collect {|fr| fr.venture_capitalist}.uniq
+    end
+
+    def big_investors
+        self.investors.select {|vc| vc.total_worth > 1_000_000_000}.uniq
+    end
 end
