9150cffbb83c0e9300ec0c6c73d1a632ccf625a9
github username <github email address>
2020-05-19 15:26:54 -0400

    Done.

--- app/models/venture_capitalist.rb
+++ app/models/venture_capitalist.rb
@@ -1,3 +1,50 @@
-class VentureCapitalist
+require ("pry")
 
+class VentureCapitalist
+  attr_reader :name, :total_worth
+  # attr_accessor
+  @@all = []
+
+  def initialize(name, total_worth)
+    @name = name
+    @total_worth = total_worth
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.tres_commas_club
+    self.all.select { |vt| vt.total_worth > 1000000000 }
+  end
+
+  def offer_contract(startup, type, investment)
+    FundingRound.new(startup, self, type, investment)
+  end
+
+  def founding_rounds
+    FundingRound.all.select do |round|
+      round.venture_capitalist == self
+    end
+  end
+
+  def portfolio
+    founding_rounds.map { |str| str.startup }.uniq
+  end
+
+  def biggest_investment
+    founding_rounds.max_by { |round| round.investment }
+  end
+
+  def invested(domain)
+    total = 0
+    founding_rounds.map do |round|
+      # binding.pry
+      if round.startup.domain == domain
+        total += round.investment
+      end
+    end
+    total
+  end
 end
--- tools/console.rb
+++ tools/console.rb
@@ -1,11 +1,39 @@
-require_relative '../config/environment.rb'
+require_relative "../config/environment.rb"
 
 def reload
-  load 'config/environment.rb'
+  load "config/environment.rb"
 end
+
+# trello = Startup.new("Trello", "Bowser")
+# twitter = Startup.new("Tweet", "Bird")
+
+st1 = Startup.new("Twitter", "Jane", "www.twitter.com")
+st2 = Startup.new("Google", "Yoshi", "www.Yoshi.com")
+st3 = Startup.new("Trello", "Bowser", "www.Trello.com")
+
+yoshi = VentureCapitalist.new("Yoshi", 2000000000)
+bowser = VentureCapitalist.new("Bowser", 1000000000010)
+toad = VentureCapitalist.new("Toad", 10000000)
+# def sign_contract(vc, inv_type, invested)
+
+st1.sign_contract(yoshi, "initial", -50_000.0)
+st1.sign_contract(yoshi, "second", 50_000)
+st2.sign_contract(toad, "initial", 50_000)
+st3.sign_contract(bowser, "initial", 100_000)
+st3.sign_contract(bowser, "sec", 100_000)
+st3.sign_contract(toad, "third", 200_000)
+st3.sign_contract(yoshi, "yoshi_round", 100_000)
+
+binding.pry
+
 # Insert code here to run before hitting the binding.pry
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
-binding.pry
-0 #leave this here to ensure binding.pry isn't the last line
+#leave this here to ensure binding.pry isn't the last line
+#
+#  through = Founding Round is the joining model, many to many relationship.
+#
+#
+#  starup -< FR >- VentureCapitalist
+#  many to many relationship
